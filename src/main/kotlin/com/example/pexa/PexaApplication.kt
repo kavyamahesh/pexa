6d1985dfd578ff7d22b9a42deb6ff04015a02d59
kavyam <kavya15.m@axisbank.com>
2025-01-30 19:50:59 +0530

    Initial commit

---
+++ src/main/kotlin/com/example/pexa/PexaApplication.kt
@@ -0,0 +1,11 @@
+package com.example.pexa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PexaApplication
+
+fun main(args: Array<String>) {
+	runApplication<PexaApplication>(*args)
+}
