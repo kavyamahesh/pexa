bf6c24575d9f2873cd1e1fb801989694e6b9b6bb
mahesha-kavya <kavya15.m@axisbank.com>
2025-01-30 20:05:00 +0530

    Add test

--- src/test/kotlin/com/example/pexa/PexaApplicationTests.kt
+++ src/test/kotlin/com/example/pexa/PexaApplicationTests.kt
@@ -2,12 +2,15 @@ package com.example.pexa
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import kotlin.test.assertEquals
+import kotlin.test.expect
 
 @SpringBootTest
 class PexaApplicationTests {
 
 	@Test
 	fun contextLoads() {
+		assertEquals(true, true)
 	}
 
 }
